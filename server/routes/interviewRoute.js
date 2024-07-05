6ddeab52c81485dfc072500c0483f789b6698e05
yaojiejia <yaojiejia0715@gmail.com>
2024-07-05 15:53:14 -0400

    gitigonore

--- server/routes/interviewRoute.js
+++ server/routes/interviewRoute.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 
 router.post("/start", async (req, res) => {
-    const {id, type, difficulty, company, k1, k2, k3, k4} = req.body;
-   
+    const {id, type, difficulty, company} = req.body;
+    
     createInterview(id, type, difficulty, company);
     
     
