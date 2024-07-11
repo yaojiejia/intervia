bb7b4dee9a24dd50f360e9029cd18009663380ea
Sewon Kim <sewonkim0black@gmail.com>
2024-07-10 22:50:41 -0400

    Client: development proxy complete, react client-side routing complete with test api fetch

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+        <button onClick={() => {
+          fetch("/api")
+            .then((res) => res.text())
+            .then((res) => alert(res));
+        }}>CALL THE API</button>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ app.use(express.json());
 app.use("/auth", authRoute);
 app.use("/start", interviewRoute);
 
+// TEST: api route for client proxy testing
+app.get("/api", (req, res) => {
+    res.send("Hello from the server!");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Server is running on port " + port);
