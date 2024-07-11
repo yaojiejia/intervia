6092a21cba56236e40cf2049d6e42137c31595f9
Sewon Kim <sewonkim0black@gmail.com>
2024-07-10 22:32:49 -0400

    Client: react setup complete

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use("/auth", authRoute);
 app.use("/start", interviewRoute);
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is running on port " + process.env.PORT);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
 });
