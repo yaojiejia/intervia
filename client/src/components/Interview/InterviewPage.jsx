0f4e77fe967ef5d9c219db8932b7bd88fca189ad
JiaxiXie <xiejiaxi2019@outlook.com>
2024-08-16 02:19:06 -0400

    Interview page with meeting window and ai dictation

--- client/src/components/Interview/InterviewPage.jsx
+++ client/src/components/Interview/InterviewPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
-import profile from './image/profile_1.png';
+
 import QuestionAndAnswer from './QAndA';
+import DictationDisplay from './AIDictation';
+import Meeting from './Meeting';
+
+import profile from './image/profile_1.png';
+import faviconImage from './image/favicon.png';
 
 const InterviewPage = () => {
     return (
@@ -14,9 +19,16 @@ const InterviewPage = () => {
                 background: 'linear-gradient(135deg, #383121 0%, #383121 40%, #000000 100%)',
             }}
         >
-            <QuestionAndAnswer/>
+            {/* <QuestionAndAnswer/>
             <Box sx={{height: '100vh', width: '50vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <img src={profile} alt="Profile" style={{ height: '45vh', }}/>
+            </Box> */}
+            <Meeting/>
+            <Box sx={{display: 'flex', flexDirection: 'column', borderLeft: '3px solid #685A4A'}}>
+                <Box sx={{ height: '40vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <img src={faviconImage} alt="Favicon" style={{ height: '25vh' }}/>
+                </Box>
+                <DictationDisplay/>
             </Box>
         </Box>
     );
