f68ef945e7d797c7269d9bcd4e716fd88d5049d5
yaojiejia <yaojiejia0715@gmail.com>
2024-09-12 22:18:43 -0400

    fixed a minor bug

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import authRoute from './routes/authRoute.js';
-import interviewRoute from './routes/interviewRoute.js';
 import dotenv from 'dotenv';
 dotenv.config();    
 const app = express();
 app.use(express.json());
 
 app.use("/auth", authRoute);
-app.use("/start", interviewRoute);
+
 
 
 const port = process.env.PORT || 5000;
