c8d38d90e2efb9e3ed1fac8a82f126208b8c6d6b
JiaxiXie <xiejiaxi2019@outlook.com>
2024-09-13 03:27:20 -0400

    Small changes on server side for testing

--- server/lib/config/startInterview.js
+++ server/lib/config/startInterview.js
@@ -1,16 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-import { getGptHistory, historyToString, getLeetCode, getTechnicalQuestion, getFirstSession, createMsg, createSession } from "../helper/dbHelper.js";
+// import { getGptHistory, historyToString, getLeetCode, getTechnicalQuestion, getFirstSession, createMsg, createSession } from "../helper/dbHelper.js";
 import {technicalQuestion, instruction} from "../script/technicalQuestion.js";
-import * as redisClient from '../redis/redis.js';
+// import * as redisClient from '../redis/redis.js';
 import dotenv from "dotenv";
-import chatGPT from "../openai/openai.js";
+// import chatGPT from "../openai/openai.js";
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
 dotenv.config();
 const prisma = new PrismaClient();
-await redisClient.connect();
+// await redisClient.connect();
 
 const app = express();
 const httpServer = createServer(app);
@@ -29,7 +29,8 @@ io.on('connection', (socket) => {
   socket.on('startInterview', async (msg) => {
     try {
       const res = await startInterview(msg, "66c4f3935abbc8813ca14411");
-      io.emit('startInterview', { msg: res, response: res.gptRes, socket: socket.id });
+      // io.emit('startInterview', { msg: res, response: res.gptRes, socket: socket.id });
+      io.emit('startInterview', { msg: res, response: 'socket receive msg', socket: socket.id });
       console.log('Message sent:', res);
     } catch (e) {
       console.log(e);
@@ -46,41 +47,42 @@ let rounds = 1;
 
 export const startInterview = async (msg, sessionId) => {
   console.log("msg coming in! " + msg);
-  await createMsg(sessionId, "user: ", msg);
+  // await createMsg(sessionId, "user: ", msg);
   if (!technicalQuestion) {
     throw new Error("Error, technical question is under");
   }
 
-  let gptRes;
+  // let gptRes;
 
-  try {
-    gptRes = await chatGPT(msg);
-    console.log(gptRes);
-  } catch (error) {
-    console.error("Error:", error);
-  }
+  // try {
+  //   gptRes = await chatGPT(msg);
+  //   console.log(gptRes);
+  // } catch (error) {
+  //   console.error("Error:", error);
+  // }
 
-  const history = await historyToString(sessionId);
+  // const history = await historyToString(sessionId);
 
-  const roundQuestions = {
-    1: technicalQuestion[0],
-    3: technicalQuestion[1],
-    6: technicalQuestion[2],
-    default: instruction + history
-  };
+  // const roundQuestions = {
+  //   1: technicalQuestion[0],
+  //   3: technicalQuestion[1],
+  //   6: technicalQuestion[2],
+  //   default: instruction + history
+  // };
 
-  const question = roundQuestions[rounds] || roundQuestions.default;
+  // const question = roundQuestions[rounds] || roundQuestions.default;
 
-  try {
-    gptRes = await chatGPT(question);
-    console.log(gptRes);
-  } catch (error) {
-    console.error("Error:", error);
-  }
+  // try {
+  //   gptRes = await chatGPT(question);
+  //   console.log(gptRes);
+  // } catch (error) {
+  //   console.error("Error:", error);
+  // }
 
   rounds++;
-  await createMsg(sessionId, "chatbot: ", gptRes);
-  return { gptRes };
+  // await createMsg(sessionId, "chatbot: ", gptRes);
+  // return { gptRes };
+  return "Test Response for --> "+msg
 }
 
 const port = 4000;
